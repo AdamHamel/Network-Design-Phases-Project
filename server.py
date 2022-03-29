89bf09453291815c4cf85bfbc6a9c4e5d62eac16
Matheus Dovale <91933211+Matt-Dovale@users.noreply.github.com>
2022-03-28 22:08:32 -0400

    Wrote Phase4 server
    Added ACK loss.

--- server.py
+++ server.py
@@ -14,14 +14,15 @@ import random
 from struct import *
 import datetime
 
-def serverSide(cRate):
-
+def serverSide(cRate, ackRate):
+    ackLoss = float(ackRate)
     corruptRate = float(cRate)
+
     serverPort = 12345
     serverSocket = socket(AF_INET, SOCK_DGRAM)
     serverSocket.bind(('', serverPort))
     numPacket = 0
-    print("Server Opened At: %s" % datetime.datetime.now())
+    # print("Server Opened At: %s" % datetime.datetime.now())
     numPackets, clientAddress = serverSocket.recvfrom(2048)
     receivedFile = []
     receiving = 0
@@ -53,6 +54,8 @@ def serverSide(cRate):
         ######## Acknowledgement System ########
 
         while True:
+
+            # All conditions met, sends next packet.
             if serverChkSum == checksum and previousSEQ != SEQ:
                 print("Server: Packet Passes.")
                 ACK = SEQ
@@ -60,20 +63,23 @@ def serverSide(cRate):
                 receivedFile.append(data)
                 receiving = receiving + 1
                 ackPacket = pack('ii', SEQ, ACK)
-                serverSocket.sendto(ackPacket, clientAddress)
+                if random.random() > ackLoss:
+                    serverSocket.sendto(ackPacket, clientAddress)
                 break
+            # Duplicate packets determined by SEQ value, sending next packet.
             elif previousSEQ == SEQ:
                 print("Server: Duplicate Packet")
                 ACK = int(not SEQ)
                 ackPacket = pack('ii', int(not SEQ), ACK)
-                serverSocket.sendto(ackPacket, clientAddress)
+                if random.random() > ackLoss:
+                    serverSocket.sendto(ackPacket, clientAddress)
                 break
+            # Packet was corrupted, determined by ACK, resending packet.
             else:
                 print("Server: Packet Corrupted.")
                 ACK = int(not SEQ)
                 ackPacket = pack('ii', SEQ, ACK)
                 serverSocket.sendto(ackPacket, clientAddress)
-
                 message, clientAddress = serverSocket.recvfrom(2048)
                 SEQ, checksum, data = unpack(f'i 32s {len(message[36:])}s', message)
                 data = corruption(corruptRate, data)
